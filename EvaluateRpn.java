ea750fa7a7656f812c7c7e3fd3efa09836eb85eb
Daniel Leskosky <dleskosky@gmail.com>
2021-01-24 19:04:32 -0500

    Commit for exercise 8.2

---
+++ EvaluateRpn.java
@@ -0,0 +1,41 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+
+import java.util.function.ToIntBiFunction;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Map;
+
+public class EvaluateRpn {
+  @EpiTest(testDataFile = "evaluate_rpn.tsv")
+
+  public static int eval(String expression) {
+    Deque<Integer> intermediateResults = new ArrayDeque<>();
+    final String DELIMITER = ",";
+    final Map<String, ToIntBiFunction<Integer, Integer>> OPERATORS = Map.of(
+            "+",
+            (y, x)
+              -> x + y,
+            "-", (y, x) -> x - y, "*", (y, x) -> x * y, "/", (y, x) -> x / y);
+    for (String token : expression.split(DELIMITER)) {
+      if (OPERATORS.get(token) != null) {
+        intermediateResults.addFirst(
+                OPERATORS.get(token).applyAsInt(intermediateResults.removeFirst(),
+                        intermediateResults.removeFirst())
+        );
+      } else {
+        intermediateResults.addFirst(Integer.parseInt(token));
+      }
+    }
+    return intermediateResults.removeFirst();
+  }
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "EvaluateRpn.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
