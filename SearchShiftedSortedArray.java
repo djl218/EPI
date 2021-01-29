0ae733e1964edd9656c3b0f2d8db8d117e950822
Daniel Leskosky <dleskosky@gmail.com>
2021-01-29 17:25:46 -0500

    Commit for exercise 11.3

---
+++ SearchShiftedSortedArray.java
@@ -0,0 +1,29 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+
+import java.util.List;
+public class SearchShiftedSortedArray {
+  @EpiTest(testDataFile = "search_shifted_sorted_array.tsv")
+
+  public static int searchSmallest(List<Integer> A) {
+    int left = 0, right = A.size() - 1;
+    while (left < right) {
+      int mid = left + ((right - left) / 2);
+      if (A.get(mid) > A.get(right)) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return left;
+  }
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "SearchShiftedSortedArray.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
