e86e10718cc124ae7e7711ef03b1d23b8da0417f
Daniel Leskosky <dleskosky@gmail.com>
2021-02-11 10:41:43 -0500

    Commit for exercise 4.8

---
+++ ReverseDigits.java
@@ -0,0 +1,22 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+public class ReverseDigits {
+  @EpiTest(testDataFile = "reverse_digits.tsv")
+  public static long reverse(int x) {
+    long result = 0;
+    while (x != 0) {
+      result = result * 10 + (x % 10);
+      x /= 10;
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "ReverseDigits.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
